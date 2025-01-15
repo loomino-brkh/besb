f579c09a0e6874144b57eb52abbc7a98f09ed55c
loomino <loomino@brkh.work>
2025-01-15 09:20:46 +0700

    Rabu, 15 Januari 2025 09:20:46 AM: Auto-commit: Changes in views.py

--- django_auth/authentication/views.py
+++ django_auth/authentication/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .permissions import LocalhostOnly
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenRefreshView
