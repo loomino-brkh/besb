3e05e5bccc6417413b2274862a80128b343ecbfd
loomino <loomino@brkh.work>
2025-01-15 08:17:12 +0700

    Rabu, 15 Januari 2025 08:17:12 AM: Auto-commit: Changes in admin.py

--- django_auth/authentication/admin.py
+++ django_auth/authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .apikey_models import APIKey
 
-# Register your models here.
+@admin.register(APIKey)
+class APIKeyAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'name', 'created_at', 'expires_at', 'revoked', 'permission')
+    list_filter = ('owner', 'revoked', 'permission')
+    search_fields = ('name', 'owner__username')
+    readonly_fields = ('key', 'hashed_key', 'created_at', 'used')
+    ordering = ('-created_at',)  # Default sorting by created_at in descending order
