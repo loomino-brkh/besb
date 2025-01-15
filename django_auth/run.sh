e7275002d6c65e0b60f8c506284e8ad2cbb52994
loomino <loomino@brkh.work>
2025-01-15 13:51:00 +0700

    Rabu, 15 Januari 2025 01:51:00 PM

--- django_auth/run.sh
+++ django_auth/run.sh
@@ -7,12 +7,12 @@ python manage.py makemigrations
 python manage.py migrate
 
 # Create superuser if it doesn't exist
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
-EOF
+#python manage.py shell <<EOF
+#from django.contrib.auth import get_user_model
+#User = get_user_model()
+#if not User.objects.filter(username='admin').exists():
+#    User.objects.create_superuser('admin', 'admin@example.com', 'admin')
+#EOF
 
 # Collect static files
 python manage.py collectstatic --noinput
