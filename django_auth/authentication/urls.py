914ed9fe62dbd001403e6d8874735904cd300803
loomino <loomino@brkh.work>
2025-01-15 08:24:39 +0700

    Rabu, 15 Januari 2025 08:24:39 AM: Auto-commit: Changes in urls.py

--- django_auth/authentication/urls.py
+++ django_auth/authentication/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('verify/', views.verify_token, name='verify'),
     path('cached/', views.cached_view, name='cached_view'),
-    path('apikeys/', apikey_view.APIKeyListView.as_view(), name='api_key_list'),
+    path('apikeys/', apikey_views.APIKeyListView.as_view(), name='api_key_list'),
     path('apikeys/create/', apikey_views.APIKeyCreateView.as_view(), name='api_key_create'),
     path('apikeys/revoke/<int:pk>/', apikey_views.APIKeyRevokeView.as_view(), name='api_key_revoke'),
 ]
