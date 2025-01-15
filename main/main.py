6fad7d85ca9a0b868eaf72eb7f2a7b654ebf2075
loomino <loomino@brkh.work>
2025-01-15 11:14:52 +0700

    Rabu, 15 Januari 2025 11:14:52 AM: Auto-commit: Changes in main.py

--- main/main.py
+++ main/main.py
@@ -40,7 +40,7 @@ app.include_router(absen_pengajian.router, prefix="/absen-pengajian", tags=["abs
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "This is API. Not web page!!"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
