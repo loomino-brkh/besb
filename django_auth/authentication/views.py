335713c5c3592d1c48331681ea1001bce5ba4cb8
loomino <loomino@brkh.work>
2025-01-15 08:37:04 +0700

    Rabu, 15 Januari 2025 08:37:04 AM: Auto-commit: Changes in views.py

--- django_auth/authentication/views.py
+++ django_auth/authentication/views.py
@@ -4,8 +4,11 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth import authenticate
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.exceptions import TokenError
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
+
 
 @api_view(['POST'])
 def login(request):
