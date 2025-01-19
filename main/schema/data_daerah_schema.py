e50f90a817f45c8e8451d299f7cda936b154ed6e
loomino <loomino@brkh.work>
2025-01-19 17:05:19 +0000

    January 20, 2025 at 12:05:19 AM GMT+7

---
+++ main/schema/data_daerah_schema.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+
+class DataDaerah(SQLModel, table=True):
+    __tablename__ = "data_daerah"
+    
+    id: int = Field(default=None, primary_key=True)
+    daerah: str = Field(index=True)
+    ranah: str
+    detail_ranah: str
