a821e617115941b863ea850911c63bd6b4a415e2
loomino <loomino@brkh.work>
2025-01-19 19:01:05 +0000

    January 20, 2025 at 2:01:05 AM GMT+7

--- main/main.py
+++ main/main.py
@@ -18,7 +18,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ngaji.brkh.work/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
