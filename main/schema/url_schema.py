7df62c7d5ef29f849b86e0f8bd4e757e6c4fba9c
loomino <loomino@brkh.work>
2025-01-21 14:38:09 +0000

    January 21, 2025 at 9:38:09 PM GMT+7

---
+++ main/schema/url_schema.py
@@ -0,0 +1,22 @@
+from sqlmodel import SQLModel, Field
+from typing import Optional
+import string
+import random
+
+def generate_code(length: int = 6) -> str:
+    characters = string.ascii_letters + string.digits
+    return ''.join(random.choice(characters) for _ in range(length))
+
+class URLBase(SQLModel):
+    url: str = Field(...)
+
+class URL(URLBase, table=True):
+    __tablename__ = "urls"
+    id: Optional[int] = Field(default=None, primary_key=True)
+    url_code: str = Field(default_factory=lambda: generate_code(), unique=True)
+
+class URLResponse(URLBase):
+    url_code: str
+
+class URLCreate(URLBase):
+    pass
