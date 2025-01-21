57dcff430e85ab408cdee8aa6cfd2969a10707d1
loomino <loomino@brkh.work>
2025-01-21 14:42:19 +0000

    January 21, 2025 at 9:42:19 PM GMT+7

--- main/main.py
+++ main/main.py
@@ -8,7 +8,7 @@ from sqlmodel import SQLModel
 import os
 import uvicorn
 from core.db import engine
-from endpoints import absen_pengajian, absen_asramaan, data_daerah, sesi
+from endpoints import absen_pengajian, absen_asramaan, data_daerah, sesi, url
 
 app = FastAPI(
     docs_url=None, 
