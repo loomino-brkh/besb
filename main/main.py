106f8a9d18b149ec2de4f81fbbf9970ae5d5fdc5
loomino <loomino@brkh.work>
2025-02-10 07:11:26 +0000

    February 10, 2025 at 2:11:26 PM GMT+7

--- main/main.py
+++ main/main.py
@@ -43,7 +43,7 @@ app.add_middleware(
 
 app.include_router(absen_pengajian.router, prefix="/absen-pengajian", tags=["absen-pengajian"])
 app.include_router(absen_asramaan.router, prefix="/absen-asramaan", tags=["absen-asramaan"])
-app.include_router(data_daerah.router, prefix="/data", tags=["data-daerah"])
+app.include_router(data_daerah.router, prefix="/data/daerah", tags=["data-daerah"])
 app.include_router(sesi.router, prefix="/sesi", tags=["sesi"])
 app.include_router(url.router, prefix="/url", tags=["url"])
 
