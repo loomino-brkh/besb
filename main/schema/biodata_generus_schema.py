27f437baaf7272b77d51d26a5afc77e3224e25ae
loomino <loomino@brkh.work>
2025-02-25 10:26:08 +0000

    February 25, 2025 at 5:26:08 PM GMT+7

---
+++ main/schema/biodata_generus_schema.py
@@ -0,0 +1,33 @@
+from typing import Optional
+from datetime import date
+from sqlmodel import SQLModel, Field
+
+class BiodataGenerusBase(SQLModel):
+    nama_lengkap: str
+    nama_panggilan: str
+    kelahiran_tempat: str
+    kelahiran_tanggal: date
+    alamat_tinggal: str
+    pendataan_tanggal: date
+    sambung_desa: str
+    sambung_kelompok: str
+    hobi: str
+    sekolah_kelas: str
+    nomor_hape: Optional[str] = None
+    nama_ayah: str
+    nama_ibu: str
+    status_ayah: str
+    status_ibu: str
+
+class BiodataGenerusCreate(BiodataGenerusBase):
+    pass
+
+class BiodataGenerusModel(BiodataGenerusBase, table=True):
+    __tablename__: str = "biodata_generus"
+    
+    id: Optional[int] = Field(default=None, primary_key=True)
+    created_at: str = Field(default_factory=lambda: date.today().isoformat())
+
+class BiodataGenerusResponse(BiodataGenerusBase):
+    id: int
+    created_at: str
