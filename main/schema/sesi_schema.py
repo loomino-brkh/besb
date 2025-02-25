d81f8019e36ac28dc299b0b49a0c5897cdc7ad22
loomino <loomino@brkh.work>
2025-02-25 11:58:45 +0000

    February 25, 2025 at 6:58:45 PM GMT+7

--- main/schema/sesi_schema.py
+++ main/schema/sesi_schema.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, Field
 from datetime import time
 
 class Sesi(SQLModel, table=True):
-    __tablename__ = "data_sesi" # type: ignore
+    __tablename__: str = "data_sesi"
     
     id: int = Field(default=None, primary_key=True)
     acara: str = Field(index=True)
