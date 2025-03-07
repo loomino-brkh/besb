4bb2a89154a6f6ce78300c64959d15df3bd63b3f
loomino <loomino@brkh.work>
2025-03-08 02:58:01 +0700

    Auto-commit: 2025-03-08 02:58:01 - biodata_generus_schema.py
    Branch: main
    File: main/schema/biodata_generus_schema.py
    1 file changed, 9 insertions(+), 1 deletion(-)
    
    Commit made automatically by System Update

--- main/schema/biodata_generus_schema.py
+++ main/schema/biodata_generus_schema.py
@@ -2,6 +2,7 @@ from typing import Optional
 from datetime import date
 from sqlmodel import SQLModel, Field
 
+
 class BiodataGenerusBase(SQLModel):
     nama_lengkap: str
     nama_panggilan: str
@@ -20,22 +21,29 @@ class BiodataGenerusBase(SQLModel):
     status_ibu: str
     nomor_hape_ayah: Optional[str] = None
     nomor_hape_ibu: Optional[str] = None
+    jenis_kelamin: str
+    daerah: str
+
 
 class BiodataGenerusCreate(BiodataGenerusBase):
     pass
 
+
 class BiodataGenerusModel(BiodataGenerusBase, table=True):
     __tablename__: str = "biodata_generus"
-    
+
     id: Optional[int] = Field(default=None, primary_key=True)
     created_at: str = Field(default_factory=lambda: date.today().isoformat())
+
 
 class BiodataGenerusResponse(BiodataGenerusBase):
     id: int
     created_at: str
+
 
 class BiodataGenerusGetResponse(SQLModel):
     nama_lengkap: str
     nama_panggilan: str
     sambung_desa: str
     sambung_kelompok: str
+    jenis_kelamin: str
