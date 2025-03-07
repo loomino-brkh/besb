cb4bdebe049ef7782a340b4b61017430c612e2ba
loomino <loomino@brkh.work>
2025-03-08 02:51:27 +0700

    Auto-commit: 2025-03-08 02:51:27 - main.py
    Branch: main
    File: main/main.py
    1 file changed, 13 insertions(+), 3 deletions(-)
    
    Commit made automatically by System Update

--- main/main.py
+++ main/main.py
@@ -21,6 +21,7 @@ from endpoints import (
     url,
 )
 
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     try:
@@ -40,6 +41,7 @@ async def lifespan(app: FastAPI):
         raise
     yield
 
+
 app = FastAPI(
     docs_url=None,
     redoc_url=None,
@@ -55,17 +57,25 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(absen_pengajian.router, prefix="/absen-pengajian", tags=["absen-pengajian"])
-app.include_router(absen_asramaan.router, prefix="/absen-asramaan", tags=["absen-asramaan"])
-app.include_router(biodata_generus.router, prefix="/biodata/generus", tags=["biodata-generus"])
+app.include_router(
+    absen_pengajian.router, prefix="/absen-pengajian", tags=["absen-pengajian"]
+)
+app.include_router(
+    absen_asramaan.router, prefix="/absen-asramaan", tags=["absen-asramaan"]
+)
+app.include_router(
+    biodata_generus.router, prefix="/biodata/generus", tags=["biodata-generus"]
+)
 app.include_router(data_daerah.router, prefix="/data/daerah", tags=["data-daerah"])
 app.include_router(sesi.router, prefix="/data/sesi", tags=["sesi"])
 app.include_router(url.router, prefix="/url", tags=["url"])
 app.include_router(data_materi.router, prefix="/data/materi", tags=["data-materi"])
 
+
 @app.get("/")
 async def root():
     return {"message": "ERRORR!!! This is API. Not web page!!"}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
